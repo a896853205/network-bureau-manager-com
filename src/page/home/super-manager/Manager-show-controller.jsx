90131e4da9062250cf48662af796fed814ed6154
Qccccc's laptop <896853205@qq.com>
2019-12-23 15:36:14 +0800

    修改manager-show的bug和竞购

--- src/page/home/super-manager/Manager-show-controller.jsx
+++ src/page/home/super-manager/Manager-show-controller.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 // 样式
-import { Icon } from 'antd';
-import { Table } from 'antd';
-import '@/style/super-manager/Manager-show.styl';
+import { Icon, Table } from 'antd';
+import '@/style/home/super-manager/manager-show.styl';
+
 const { Column } = Table;
 
 export default props => {
   const data = [];
   for (let i = 0; i < 50; i++) {
     data.push({
+      uuid: i,
       username: `test${i}`,
       name: `测试${i}号`,
       phone: 18351923820,
@@ -18,8 +19,8 @@ export default props => {
   }
 
   return (
-    <dev className='manager-show-box'>
-      <Table dataSource={data} className='table'>
+    <div className='manager-show-box'>
+      <Table dataSource={data} className='table' rowKey={record => record.uuid}>
         <Column title='账号' dataIndex='username' key='username' />
         <Column title='姓名' dataIndex='name' key='name' />
         <Column title='电话' dataIndex='phone' key='phone' />
@@ -30,16 +31,16 @@ export default props => {
           key='operations'
           render={(icon1, icon2) => (
             <span className='icon-box'>
-              <a>
+              <a href='/'>
                 <Icon type='edit' className='icon' />
               </a>
-              <a>
+              <a href='/'>
                 <Icon type='delete' className='icon' />
               </a>
             </span>
           )}
         />
       </Table>
-    </dev>
+    </div>
   );
 };
