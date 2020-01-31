d17ae4bb9847b41f35c555db95e3b29a781de45e
Zbr1920410104 <765451419@qq.com>
2020-01-31 16:05:05 +0800

    添加了上传四个文件的样式

--- src/components/home/project-manager/registration-detail/Document.jsx
+++ src/components/home/project-manager/registration-detail/Document.jsx
@@ -1,5 +1,44 @@
 import React from 'react';
 
+// 路由
+import { HOME_REGISTRATION_PROFILE } from '@/constants/route-constants';
+import { Link } from 'react-router-dom';
+
+//样式
+import { Icon, Button, Input } from 'antd';
+import '@/style/home/project-manager/document.styl';
+const { TextArea } = Input;
+
 export default props => {
-  return <></>;
+  return (
+    <>
+      <div className='subtitle-box'>
+        <Link to={`${HOME_REGISTRATION_PROFILE.path}`}>
+          <Icon type='left' className='exit-icon' />
+        </Link>
+        <p className='subtitle-title'>用户文档集</p>
+      </div>
+      <div className='detail-document-box'>
+        <div className='document-upload-button-box'>
+          <Button type='primary' icon='download'>
+            下载文件
+          </Button>
+        </div>
+        <div className='document-button-box'>
+          <Button type='primary' htmlType='submit' className='fail-button'>
+            审核不通过
+          </Button>
+          <Button type='primary' htmlType='submit' className='success-button'>
+            审核通过
+          </Button>
+        </div>
+        <TextArea
+          autoSize={{ minRows: 3, maxRows: 6 }}
+          maxLength='800'
+          placeholder='请输入审核不通过理由'
+          className='document-textArea-box'
+        />
+      </div>
+    </>
+  );
 };
