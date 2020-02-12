6517ea2a074ea94475126f748327189c00059a37
Qccccc's laptop <896853205@qq.com>
2020-02-13 00:49:34 +0800

    完成部分颜色把控

--- src/components/home/project-manager/registration-process/Electronic-contract-profile.jsx
+++ src/components/home/project-manager/registration-process/Electronic-contract-profile.jsx
@@ -21,38 +21,16 @@ export default () => {
       enterpriseRegistrationUuid,
       sysRegistrationStep
     } = useSelector(state => state.enterpriseStore),
-    [managerStatus, setManagerStatus] = useState([]),
-    [pushProcessLoading, setPushProcessLoading] = useState(false),
-    [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (enterpriseRegistrationUuid) {
-      (async () => {
-        setLoading(true);
-        const res = await proxyFetch(
-          SELECT_REGISTRATION_CONTRACT_MANAGER,
-          { registrationUuid: enterpriseRegistrationUuid },
-          'GET'
-        );
-
-        setManagerStatus(res.managerStatus);
-        setLoading(false);
-      })();
-    }
-  }, [enterpriseRegistrationUuid]);
+    [pushProcessLoading, setPushProcessLoading] = useState(false);
 
   const contractManagerStatusToColor = (step, managerStatus = 0) => {
-    let color = '';
-
     if (managerStatus === step) {
-      color = 'blue';
+      return 'blue';
     } else if (managerStatus > step) {
-      color = 'green';
+      return 'green';
     } else {
-      color = 'grey';
+      return 'grey';
     }
-
-    return color;
   };
 
   const handlePushProcess = () => {
@@ -74,79 +52,69 @@ export default () => {
         theme='twoTone'
         twoToneColor='#334454'
       />
-      <Skeleton loading={loading}>
-        {sysRegistrationStep.length ? (
-          <div className='item-text-box'>
-            <div className='text-top-box'>
-              {sysRegistrationStep[1].name}
-              <Tag className='title-tag' color={steps[1].color}>
-                {steps[1].statusText}
-              </Tag>
-            </div>
-            <div className='item-detail-box'>
-              <p className='text-subtitle'>甲乙双方电子签合同</p>
-              <Timeline mode='left'>
-                <Timeline.Item
-                  color={contractManagerStatusToColor(1, managerStatus)}
-                >
-                  {managerStatus ? (
-                    <Link
-                      to={`${HOME_REGISTRATION_DETAIL.path}/contractManager`}
-                    >
-                      <span>填写经营管理部合同内容</span>
-                    </Link>
-                  ) : (
-                    <span>填写经营管理部合同内容</span>
-                  )}
-                </Timeline.Item>
-                <Timeline.Item
-                  color={contractManagerStatusToColor(2, managerStatus)}
-                >
-                  {managerStatus >= 2 ? (
-                    <Link
-                      to={`${HOME_REGISTRATION_DETAIL.path}/contractDownload`}
-                    >
-                      <span>管理员制定合同</span>
-                    </Link>
-                  ) : (
-                    <span>管理员制定合同</span>
-                  )}
-                </Timeline.Item>
-                <Timeline.Item
-                  color={contractManagerStatusToColor(3, managerStatus)}
-                >
-                  等待企业上传合同pdf
-                </Timeline.Item>
-                <Timeline.Item
-                  color={
-                    managerStatus === 6
-                      ? 'red'
-                      : contractManagerStatusToColor(4, managerStatus)
-                  }
-                >
-                  {managerStatus >= 4 ? (
-                    <Link
-                      to={`${HOME_REGISTRATION_DETAIL.path}/contractExamine`}
-                    >
-                      <span>审查最终合同</span>
-                    </Link>
-                  ) : (
-                    <span>审查最终合同</span>
-                  )}
-                </Timeline.Item>
-              </Timeline>
-            </div>
-            <Button
-              disabled={managerStatus !== 5}
-              size='large'
-              onClick={handlePushProcess}
-              loading={pushProcessLoading}
-            >
-              电子签合同完成开始交付汇款
-            </Button>
+      {sysRegistrationStep.length ? (
+        <div className='item-text-box'>
+          <div className='text-top-box'>
+            {sysRegistrationStep[1].name}
+            <Tag className='title-tag' color={steps[1].color}>
+              {steps[1].statusText}
+            </Tag>
           </div>
-        ) : null}
-      </Skeleton>
+          <div className='item-detail-box'>
+            <p className='text-subtitle'>甲乙双方电子签合同</p>
+            <Timeline mode='left'>
+              <Timeline.Item
+                color={contractManagerStatusToColor(1, steps[1].status)}
+              >
+                {steps[1].status >= 1 ? (
+                  <Link to={`${HOME_REGISTRATION_DETAIL.path}/contractManager`}>
+                    <span>填写经营管理部合同内容</span>
+                  </Link>
+                ) : (
+                  <span>填写经营管理部合同内容</span>
+                )}
+              </Timeline.Item>
+              <Timeline.Item
+                color={contractManagerStatusToColor(2, steps[1].status)}
+              >
+                {steps[1].status >= 2 ? (
+                  <Link
+                    to={`${HOME_REGISTRATION_DETAIL.path}/contractDownload`}
+                  >
+                    <span>管理员制定合同</span>
+                  </Link>
+                ) : (
+                  <span>管理员制定合同</span>
+                )}
+              </Timeline.Item>
+              <Timeline.Item
+                color={contractManagerStatusToColor(3, steps[1].status)}
+              >
+                等待企业上传合同pdf
+              </Timeline.Item>
+              <Timeline.Item
+                color={contractManagerStatusToColor(4, steps[1].status)}
+              >
+                {steps[1].status >= 4 ? (
+                  <Link to={`${HOME_REGISTRATION_DETAIL.path}/contractExamine`}>
+                    <span>审查最终合同</span>
+                  </Link>
+                ) : (
+                  <span>审查最终合同</span>
+                )}
+              </Timeline.Item>
+            </Timeline>
+          </div>
+          <Button
+            disabled={steps[1].status !== 100}
+            size='large'
+            onClick={handlePushProcess}
+            loading={pushProcessLoading}
+          >
+            电子签合同完成开始交付汇款
+          </Button>
+        </div>
+      ) : null}
     </div>
   );
 };
