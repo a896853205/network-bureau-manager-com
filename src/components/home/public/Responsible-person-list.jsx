9831d5d3d6830046b0cc9b66afc35551ee724adc
Qccccc's laptop <896853205@qq.com>
2020-02-16 22:24:45 +0800

    完成负责人列表的布局

--- src/components/home/public/Responsible-person-list.jsx
+++ src/components/home/public/Responsible-person-list.jsx
@@ -1,5 +1,70 @@
 import React from 'react';
 
+// 样式
+import '@/style/home/item.styl';
+import '@/style/home/responsible-person-list.styl';
+
 export default props => {
-  return <></>;
+  return (
+    <div className='item-box'>
+      <h5 className='title-box'>负责人列表</h5>
+      <ul className='responsible-person-ul'>
+        <li>
+          <img
+            src='https://c-ssl.duitang.com/uploads/item/201511/21/20151121171107_zMZcy.jpeg'
+            alt=''
+          />
+          <div className='responsible-person-card-description'>
+            <span>钱程</span>
+            <span>项目管理员</span>
+            <span>15998133472</span>
+          </div>
+        </li>
+        <li>
+          <img
+            src='https://c-ssl.duitang.com/uploads/item/201511/21/20151121171107_zMZcy.jpeg'
+            alt=''
+          />
+          <div className='responsible-person-card-description'>
+            <span>钱程</span>
+            <span>项目管理员</span>
+            <span>15998133472</span>
+          </div>
+        </li>{' '}
+        <li>
+          <img
+            src='https://c-ssl.duitang.com/uploads/item/201511/21/20151121171107_zMZcy.jpeg'
+            alt=''
+          />
+          <div className='responsible-person-card-description'>
+            <span>钱程</span>
+            <span>项目管理员</span>
+            <span>15998133472</span>
+          </div>
+        </li>{' '}
+        <li>
+          <img
+            src='https://c-ssl.duitang.com/uploads/item/201511/21/20151121171107_zMZcy.jpeg'
+            alt=''
+          />
+          <div className='responsible-person-card-description'>
+            <span>钱程</span>
+            <span>项目管理员</span>
+            <span>15998133472</span>
+          </div>
+        </li>{' '}
+        <li>
+          <img
+            src='https://c-ssl.duitang.com/uploads/item/201511/21/20151121171107_zMZcy.jpeg'
+            alt=''
+          />
+          <div className='responsible-person-card-description'>
+            <span>钱程</span>
+            <span>项目管理员</span>
+            <span>15998133472</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 };
