408b2ed2b6769e4599040f36e05f612cad610a2a
Qccccc's laptop <896853205@qq.com>
2020-02-17 22:27:43 +0800

    完成下载产品介质路由

--- src/components/home/public/File-download-list.jsx
+++ src/components/home/public/File-download-list.jsx
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 // redux
 import { useSelector } from 'react-redux';
 
 // 请求
-import {
-  GET_FILE_URL,
-  GET_REGISTRATION_FILE_BY_FILE_DOWNLOAD_REGISTRATION_UUID
-} from '@/constants/api-constants';
+import { DOWNLOAD_PRODUCT } from '@/constants/api-constants';
 import proxyFetch from '@/util/request';
 
 // 样式
@@ -19,74 +16,25 @@ export default props => {
   const { fileDownloadRegistrationUuid } = useSelector(
       state => state.enterpriseStore
     ),
-    [copyrightUrl, setCopyrightUrl] = useState(''),
-    [productDescriptionUrl, setProductDescriptionUrl] = useState(''),
-    [documentUrl, setDocumentUrl] = useState(''),
-    [productUrl, setProductUrl] = useState(''),
-    [CopyrightPreviewUrl, setCopyrightPreviewUrl] = useState(''),
-    [productDescriptionPreviewUrl, setProductDescriptionPreviewUrl] = useState(
-      ''
-    ),
-    [documentPreviewUrl, setDocumentPreviewUrl] = useState(''),
-    [productPreviewUrl, setProductPreviewUrl] = useState(''),
-    [getFileLoading, setGetFileLoading] = useState(true);
+    [productDownloadLoading, setProductDownloadLoading] = useState(false);
 
-  // 将已有的数据回显
-  useEffect(() => {
-    if (fileDownloadRegistrationUuid) {
-      (async () => {
-        setGetFileLoading(true);
+  const handleDownloadProduct = async () => {
+    setProductDownloadLoading(true);
 
-        let {
-          copyrightUrl,
-          productDescriptionUrl,
-          documentUrl,
-          productUrl
-        } = await proxyFetch(
-          GET_REGISTRATION_FILE_BY_FILE_DOWNLOAD_REGISTRATION_UUID,
-          { registrationUuid: fileDownloadRegistrationUuid },
-          'GET'
-        );
-
-        setCopyrightUrl(copyrightUrl);
-        setProductDescriptionUrl(productDescriptionUrl);
-        setDocumentUrl(documentUrl);
-        setProductUrl(productUrl);
-        setGetFileLoading(false);
-      })();
-    }
-  }, [fileDownloadRegistrationUuid]);
-
-  useEffect(() => {
-    if (copyrightUrl) {
-      (async () => {
-        setGetFileLoading(true);
-        const [
-          CopyrightPreviewUrl,
-          productDescriptionPreviewUrl,
-          documentPreviewUrl,
-          productPreviewUrl
-        ] = await Promise.all([
-          proxyFetch(GET_FILE_URL, { fileUrl: copyrightUrl }, 'GET'),
-          proxyFetch(GET_FILE_URL, { fileUrl: productDescriptionUrl }, 'GET'),
-          proxyFetch(GET_FILE_URL, { fileUrl: documentUrl }, 'GET'),
-          proxyFetch(GET_FILE_URL, { fileUrl: productUrl }, 'GET')
-        ]);
-
-        setCopyrightPreviewUrl(CopyrightPreviewUrl);
-        setProductDescriptionPreviewUrl(productDescriptionPreviewUrl);
-        setDocumentPreviewUrl(documentPreviewUrl);
-        setProductPreviewUrl(productPreviewUrl);
-        setGetFileLoading(false);
-      })();
-    }
-  }, [copyrightUrl, productDescriptionUrl, documentUrl, productUrl]);
+    const url = await proxyFetch(
+      DOWNLOAD_PRODUCT,
+      { registrationUuid: fileDownloadRegistrationUuid },
+      'GET'
+    );
+    setProductDownloadLoading(false);
+    window.open(url, '_blank');
+  };
 
   return (
     <div className='item-box'>
       <h5 className='title-box'>登记测试内容文件详情</h5>
       <div>
-        <ul className='file-ul' loading={getFileLoading}>
+        <ul className='file-ul'>
           <li>
             <Icon
               className='file-title-icon'
@@ -96,27 +44,14 @@ export default props => {
             评测合同下载
           </li>
           <li>
-            {copyrightUrl ? (
-              <a href={CopyrightPreviewUrl}>
-                <div>
-                  <Icon
-                    className='file-title-icon'
-                    type='reconciliation'
-                    theme='twoTone'
-                  />
-                  <span>软件著作权证书下载</span>
-                </div>
-              </a>
-            ) : (
-              <div>
-                <Icon
-                  className='file-title-icon'
-                  type='reconciliation'
-                  theme='twoTone'
-                />
-                <span>软件著作权证书下载</span>
-              </div>
-            )}
+            <div>
+              <Icon
+                className='file-title-icon'
+                type='reconciliation'
+                theme='twoTone'
+              />
+              <span>软件著作权证书下载</span>
+            </div>
           </li>
           <li>
             <Icon
@@ -127,73 +62,37 @@ export default props => {
             <span>样品登记表下载</span>
           </li>
           <li>
-            {productDescriptionUrl ? (
-              <a
-                href={productDescriptionPreviewUrl}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <div>
-                  <Icon
-                    className='file-title-icon'
-                    type='reconciliation'
-                    theme='twoTone'
-                  />
-                  <span>产品说明下载</span>
-                </div>
-              </a>
-            ) : (
+            <div>
+              <Icon
+                className='file-title-icon'
+                type='reconciliation'
+                theme='twoTone'
+              />
+              <span>产品说明下载</span>
+            </div>
+          </li>
+          <li>
+            <div>
+              <Icon
+                className='file-title-icon'
+                type='reconciliation'
+                theme='twoTone'
+              />
+              <span>用户文档集下载</span>
+            </div>
+          </li>
+          <li>
+            {productDownloadLoading ? (
               <div>
                 <Icon
                   className='file-title-icon'
-                  type='reconciliation'
-                  theme='twoTone'
+                  type='loading'
+                  spin
                 />
-                <span>产品说明下载</span>>
+                <span>请等待...</span>
               </div>
-            )}
-          </li>
-          <li>
-            {documentUrl ? (
-              <a
-                href={documentPreviewUrl}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <div>
-                  <Icon
-                    className='file-title-icon'
-                    type='reconciliation'
-                    theme='twoTone'
-                  />
-                  <span>用户文档集下载</span>
-                </div>
-              </a>
             ) : (
-              <div>
-                <Icon
-                  className='file-title-icon'
-                  type='reconciliation'
-                  theme='twoTone'
-                />
-                <span>用户文档集下载</span>
-              </div>
-            )}
-          </li>
-          <li>
-            {productUrl ? (
-              <a href={productPreviewUrl}>
-                <div>
-                  <Icon
-                    className='file-title-icon'
-                    type='reconciliation'
-                    theme='twoTone'
-                  />
-                  <span>产品介质下载</span>
-                </div>
-              </a>
-            ) : (
-              <div>
+              <div onClick={handleDownloadProduct}>
                 <Icon
                   className='file-title-icon'
                   type='reconciliation'
