0f9ae37409eb6f4c5226ed48d3a828e18ce4f61a
Zbr1920410104 <765451419@qq.com>
2020-02-17 21:15:00 +0800

    完成文件修改页面

--- src/components/home/public/File-download-list.jsx
+++ src/components/home/public/File-download-list.jsx
@@ -20,47 +20,73 @@ export default props => {
       state => state.enterpriseStore
     ),
     [copyrightUrl, setCopyrightUrl] = useState(''),
+    [productDescriptionUrl, setProductDescriptionUrl] = useState(''),
+    [documentUrl, setDocumentUrl] = useState(''),
+    [productUrl, setProductUrl] = useState(''),
     [CopyrightPreviewUrl, setCopyrightPreviewUrl] = useState(''),
-    [getCopyrightFileLoading, setGetCopyrightFileLoading] = useState(true);
-/*
+    [productDescriptionPreviewUrl, setProductDescriptionPreviewUrl] = useState(
+      ''
+    ),
+    [documentPreviewUrl, setDocumentPreviewUrl] = useState(''),
+    [productPreviewUrl, setProductPreviewUrl] = useState(''),
+    [getFileLoading, setGetFileLoading] = useState(true);
+
   // 将已有的数据回显
   useEffect(() => {
     if (fileDownloadRegistrationUuid) {
       (async () => {
-        setGetCopyrightFileLoading(true);
+        setGetFileLoading(true);
 
-        let { copyrightUrl, productDescriptionUrl, documentUrl, productUrl } = await proxyFetch(
+        let {
+          copyrightUrl,
+          productDescriptionUrl,
+          documentUrl,
+          productUrl
+        } = await proxyFetch(
           GET_REGISTRATION_FILE_BY_FILE_DOWNLOAD_REGISTRATION_UUID,
           { registrationUuid: fileDownloadRegistrationUuid },
           'GET'
         );
 
         setCopyrightUrl(copyrightUrl);
-        setGetCopyrightFileLoading(false);
+        setProductDescriptionUrl(productDescriptionUrl);
+        setDocumentUrl(documentUrl);
+        setProductUrl(productUrl);
+        setGetFileLoading(false);
       })();
     }
   }, [fileDownloadRegistrationUuid]);
-*/
+
   useEffect(() => {
     if (copyrightUrl) {
       (async () => {
-        setGetCopyrightFileLoading(true);
-        const previewUrl = await proxyFetch(
-          GET_FILE_URL,
-          { fileUrl: copyrightUrl },
-          'GET'
-        );
+        setGetFileLoading(true);
+        const [
+          CopyrightPreviewUrl,
+          productDescriptionPreviewUrl,
+          documentPreviewUrl,
+          productPreviewUrl
+        ] = await Promise.all([
+          proxyFetch(GET_FILE_URL, { fileUrl: copyrightUrl }, 'GET'),
+          proxyFetch(GET_FILE_URL, { fileUrl: productDescriptionUrl }, 'GET'),
+          proxyFetch(GET_FILE_URL, { fileUrl: documentUrl }, 'GET'),
+          proxyFetch(GET_FILE_URL, { fileUrl: productUrl }, 'GET')
+        ]);
 
-        setCopyrightPreviewUrl(previewUrl);
-        setGetCopyrightFileLoading(false);
+        setCopyrightPreviewUrl(CopyrightPreviewUrl);
+        setProductDescriptionPreviewUrl(productDescriptionPreviewUrl);
+        setDocumentPreviewUrl(documentPreviewUrl);
+        setProductPreviewUrl(productPreviewUrl);
+        setGetFileLoading(false);
       })();
     }
-  }, [copyrightUrl]);
+  }, [copyrightUrl, productDescriptionUrl, documentUrl, productUrl]);
+
   return (
     <div className='item-box'>
       <h5 className='title-box'>登记测试内容文件详情</h5>
       <div>
-        <ul className='file-ul'>
+        <ul className='file-ul' loading={getFileLoading}>
           <li>
             <Icon
               className='file-title-icon'
@@ -72,13 +98,13 @@ export default props => {
           <li>
             {copyrightUrl ? (
               <a href={CopyrightPreviewUrl}>
-                <div loading={getCopyrightFileLoading}>
+                <div>
                   <Icon
                     className='file-title-icon'
                     type='reconciliation'
                     theme='twoTone'
                   />
-                  软件著作权证书下载
+                  <span>软件著作权证书下载</span>
                 </div>
               </a>
             ) : (
@@ -88,7 +114,7 @@ export default props => {
                   type='reconciliation'
                   theme='twoTone'
                 />
-                软件著作权证书下载
+                <span>软件著作权证书下载</span>
               </div>
             )}
           </li>
@@ -98,7 +124,84 @@ export default props => {
               type='reconciliation'
               theme='twoTone'
             />
-            样品登记表下载
+            <span>样品登记表下载</span>
+          </li>
+          <li>
+            {productDescriptionUrl ? (
+              <a
+                href={productDescriptionPreviewUrl}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <div>
+                  <Icon
+                    className='file-title-icon'
+                    type='reconciliation'
+                    theme='twoTone'
+                  />
+                  <span>产品说明下载</span>
+                </div>
+              </a>
+            ) : (
+              <div>
+                <Icon
+                  className='file-title-icon'
+                  type='reconciliation'
+                  theme='twoTone'
+                />
+                <span>产品说明下载</span>>
+              </div>
+            )}
+          </li>
+          <li>
+            {documentUrl ? (
+              <a
+                href={documentPreviewUrl}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <div>
+                  <Icon
+                    className='file-title-icon'
+                    type='reconciliation'
+                    theme='twoTone'
+                  />
+                  <span>用户文档集下载</span>
+                </div>
+              </a>
+            ) : (
+              <div>
+                <Icon
+                  className='file-title-icon'
+                  type='reconciliation'
+                  theme='twoTone'
+                />
+                <span>用户文档集下载</span>
+              </div>
+            )}
+          </li>
+          <li>
+            {productUrl ? (
+              <a href={productPreviewUrl}>
+                <div>
+                  <Icon
+                    className='file-title-icon'
+                    type='reconciliation'
+                    theme='twoTone'
+                  />
+                  <span>产品介质下载</span>
+                </div>
+              </a>
+            ) : (
+              <div>
+                <Icon
+                  className='file-title-icon'
+                  type='reconciliation'
+                  theme='twoTone'
+                />
+                <span>产品介质下载</span>
+              </div>
+            )}
           </li>
           <li>
             <Icon
@@ -106,31 +209,7 @@ export default props => {
               type='reconciliation'
               theme='twoTone'
             />
-            产品说明下载
-          </li>
-          <li>
-            <Icon
-              className='file-title-icon'
-              type='reconciliation'
-              theme='twoTone'
-            />
-            用户文档集下载
-          </li>
-          <li>
-            <Icon
-              className='file-title-icon'
-              type='reconciliation'
-              theme='twoTone'
-            />
-            产品介质下载
-          </li>
-          <li>
-            <Icon
-              className='file-title-icon'
-              type='reconciliation'
-              theme='twoTone'
-            />
-            现场测试申请表下载
+            <span>现场测试申请表下载</span>
           </li>
         </ul>
       </div>
