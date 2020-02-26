d02bf444f0b9014ebf7fbd2fd34ccf2304c11b39
Qccccc's laptop <896853205@qq.com>
2020-02-26 10:23:01 +0800

    完成图片压缩webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -364,11 +364,21 @@ module.exports = function(webpackEnv) {
             // A missing `test` is equivalent to a match.
             {
               test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-              loader: require.resolve('url-loader'),
-              options: {
-                limit: imageInlineSizeLimit,
-                name: 'static/media/[name].[hash:8].[ext]'
-              }
+              use: [
+                {
+                  loader: require.resolve('url-loader'),
+                  options: {
+                    limit: imageInlineSizeLimit,
+                    name: 'static/media/[name].[hash:8].[ext]'
+                  }
+                },
+                {
+                  loader: require.resolve('image-webpack-loader'),
+                  options: {
+                    bypassOnDebug: true
+                  }
+                }
+              ]
             },
             // 配置stylus文件
             {
