4e638f1fa5fc069fab09592780179b6c6d3cbfe9
Zbr1920410104 <765451419@qq.com>
2020-05-30 13:14:13 +0800

    修改端口

--- src/constants/domin-constants.js
+++ src/constants/domin-constants.js
@@ -3,9 +3,9 @@ import { SAP_CONTROL } from '../config/app-config';
 
 /** 域名 */
 const _DOMAIN = {
-  [ENVIRONMENT.DEV]: 'http://localhost:4400',
-  [ENVIRONMENT.TEST]: 'http://localhost:4400',
-  [ENVIRONMENT.PRO]: 'http://39.97.175.30:4400'
+  [ENVIRONMENT.DEV]: 'http://localhost:4401',
+  [ENVIRONMENT.TEST]: 'http://localhost:4401',
+  [ENVIRONMENT.PRO]: 'http://39.97.175.30:4401'
 };
 
 export const DOMAIN = _DOMAIN[SAP_CONTROL];
